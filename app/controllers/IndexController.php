70554db0033a28632e82295a7d41afc2f788d6fd
heropoo <aiyouyou1000@163.com>
2017-03-04 01:01:56 +0800

    v0.4

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace app\controllers;
+use coco\web\Controller;
 
 /**
  * Index
  * Date: 2015/11/17
  * Time: 18:12
  */
-class IndexController
+class IndexController extends Controller
 {
-
+    public function indexAction(){
+        return $this->render('index/index');
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,17 +1,14 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('COCO_DEBUG') or define('COCO_DEBUG', true);
-defined('COCO_ENV') or define('COCO_ENV', 'dev');
+//defined('COCO_DEBUG') or define('COCO_DEBUG', true);
+//defined('COCO_ENV') or define('COCO_ENV', 'dev');
 
 // include composer autoload
 require(__DIR__ . '/../vendor/autoload.php');
 
-// CoCo entrance
-require(__DIR__ . '/../vendor/coco-framework/coco/CoCo.php');
-
-// include this application main config file
-$config = require(__DIR__ . '/../config/app.php');
+require_once '../vendor/coco-soft/coco-framework/config/helpers.php';
 
 // new and run a CoCo public application
-(new coco\web\Application($config))->run();
+$app = new coco\web\Application(dirname(__DIR__));
+$app->run();
